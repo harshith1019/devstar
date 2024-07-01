0aefb2e35f7f7022234faa930756d96d5e7e8c92
subitha12 <velikantisubitha@gmail.com>
2024-07-01 23:32:49 +0530

    Create App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+    import ImageEditor from './src/routes/(tools)/image-editor/+page.svelte';
+  </script>
+  
+  <main>
+    <h1>Book Your Appointment Now</h1>
+    <ImageEditor />
+  </main>
+  
+  <style>
+    main {
+      text-align: center;
+    }
+  
+    h1 {
+      margin-bottom: 20px;
+    }
+  </style>
+  
