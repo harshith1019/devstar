220b0f92187a874238c9529c96600715a7714a94
Ajayraj Rathod <144706415+ajayrajrthd@users.noreply.github.com>
2024-07-04 22:37:29 +0530

    Update +page.svelte

--- src/routes/(tools)/image-editor/+page.svelte
+++ src/routes/(tools)/image-editor/+page.svelte
@@ -1,27 +1,3 @@
-<!-- <script>
-
-</script>
-
-<div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
-	Add tool here
-</div>
-
-<style>
-
-</style> -->
-
-
-
-
-
-
-
-
-
-
-
-
-
 <script>
   import { onMount } from 'svelte';
 
