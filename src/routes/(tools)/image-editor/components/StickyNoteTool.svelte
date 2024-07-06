fecdea78499ed9568e7e08d02ab96173d302c216
subitha12 <velikantisubitha@gmail.com>
2024-07-06 18:27:09 +0530

    Create StickyNoteTool.svelte

---
+++ src/routes/(tools)/image-editor/components/StickyNoteTool.svelte
@@ -0,0 +1,31 @@
+<script>
+    let notes = [];
+    let text = '';
+
+    function addNote() {
+        notes.push({ x: 100, y: 100, text });
+        text = '';
+    }
+</script>
+
+<style>
+    .note {
+        position: absolute;
+        background: yellow;
+        padding: 10px;
+        border: 1px solid black;
+        resize: both;
+        overflow: auto;
+    }
+</style>
+
+<div>
+    <textarea bind:value={text} placeholder="Enter note"></textarea>
+    <button on:click={addNote}>Add Note</button>
+</div>
+
+{#each notes as { x, y, text }}
+    <div class="note" style="left: {x}px; top: {y}px;">
+        {text}
+    </div>
+{/each}
